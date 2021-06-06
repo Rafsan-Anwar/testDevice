3c059e4a1ba9349d58c8b9e878c705d72a58ec1b
MoinulHossainNabil <nnaabiill@gmail.com>
2021-06-06 17:50:01 +0600

    updated

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from .views import TestApi1, TestApi2
 urlpatterns = [
     path('test-api1/', TestApi1.as_view(), name='TestApi1'),
     path('test-api1/<int:pk>/', TestApi1.as_view(), name='TestApi1WithPk'),
-    path('test-api2/', TestApi2.as_view(), name='TestApi2WithPk'),
-    path('test-api2/<int:pk>/', TestApi2.as_view(), name='TestApi2'),
+    path('test-api2/', TestApi2.as_view(), name='TestApi2'),
+    path('test-api2/<int:pk>/', TestApi2.as_view(), name='TestApi2WithPk'),
 ]
